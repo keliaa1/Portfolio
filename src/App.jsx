384f6ab511e87d14e21dd70b007abc734c9537ca
Kelia Simbi <simbikelia@gmail.com>
2025-10-05 21:43:18 +0200

    feat: add navigation bar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
