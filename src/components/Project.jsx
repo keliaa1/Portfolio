0504ac6a0fa9473ff1d996e3702990759544f56a
Kelia Simbi <simbikelia@gmail.com>
2025-10-13 18:36:14 +0200

    Fix: Images displaying issue

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,29 +1,41 @@
-import { span } from 'motion/react-client'
-import React from 'react'
-import ProjectDetails from './ProjectDetails'
+import React, { useState } from "react";
+import ProjectDetails from "./ProjectDetails";
 
-const Project = ({title, description, subDescription, href, image, tags}) => {
+const Project = ({ title, description, subDescription, href, image, tags }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-    <div className='flex-wrap items-center py-10 justify-between space-y-14 sm:flex sm:space-y-0'>
+      <div className="flex-wrap items-center py-10 justify-between space-y-14 sm:flex sm:space-y-0">
         <div>
-      <h2 className='text-4xl'>{title}</h2>
-      <div className='flex gap-5 text-sand mt-2'>
-       {tags.map((tag)=>(
-        <span key={tag.id}>{tag.name}</span>
-       ))}
+          <h2 className="text-4xl">{title}</h2>
+          <div className="flex gap-5 text-sand mt-2">
+            {tags.map((tag) => (
+              <span key={tag.id}>{tag.name}</span>
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={() => setIsOpen(true)}
+          className="flex items-center gap-1 cursor-pointer hover-animation"
+        >
+          Read more
+          <img src="/logos/right-arrow.svg" alt="" className="w-5" />
+        </button>
       </div>
-      </div>
-      <button className='flex items-center gap-1 cursor-pointer hover-animation'>
-        Read more
-        <img src="public/logos/right-arrow.svg" alt="" className='w-5' />
-      </button>
-      
-    </div>
-    <div className='bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full' />
-    <ProjectDetails title={title} description={description} subDescription={subDescription} href={href} image={image} />
+      <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      {isOpen && (
+        <ProjectDetails
+          title={title}
+          description={description}
+          subDescription={subDescription}
+          href={href}
+          image={image}
+          tags={tags}
+          onClose={() => setIsOpen(false)}
+        />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
