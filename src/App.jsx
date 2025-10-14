6fdcb8d2981f919906f8705cc5bbfa7eea199f77
Kelia Simbi <simbikelia@gmail.com>
2025-10-14 06:22:00 +0200

    Feat: Project section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
+import Experience from './sections/Experience'
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Experience />
  
     </div>
   )
